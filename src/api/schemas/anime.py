6234aa688e1ff427511ca3dbe2e287f2b491d8c7
dc-mkacprzak <michael@datacose.com>
2022-12-20 23:59:44 +0100

    add description to website_url field

--- src/api/schemas/anime.py
+++ src/api/schemas/anime.py
@@ -1,21 +1,22 @@
 import uuid
 from typing import Optional
 
-from pydantic import HttpUrl, validator
+from pydantic import HttpUrl, validator, Field
 
 from .model import Model
+
+ANIME_WEBSITE_DOMAIN = 'wbijam.pl'
 
 
 class AnimeBase(Model):
     name: str
-    website_url: HttpUrl
-    last_episode: Optional[int]
+    website_url: HttpUrl = Field(description=f"Must be URL from {ANIME_WEBSITE_DOMAIN!r} domain")
+    last_episode: Optional[int] = 0
 
     @validator('website_url')
     def passwords_match(cls, website_url):
-        WEBSITE_DOMAIN = 'wbijam.pl'
-        if WEBSITE_DOMAIN not in website_url:
-            raise ValueError(f"{website_url} not from {WEBSITE_DOMAIN} domain")
+        if ANIME_WEBSITE_DOMAIN not in website_url:
+            raise ValueError(f"{website_url} not from {ANIME_WEBSITE_DOMAIN!r} domain")
         return website_url
 
 
