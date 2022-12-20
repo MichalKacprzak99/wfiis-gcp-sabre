fb52072ac388130e8a4e759964c1df6fe4f9ef1a
dc-mkacprzak <michael@datacose.com>
2022-12-20 15:10:13 +0100

    setup google cloud logging

--- src/api/main.py
+++ src/api/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+import google.cloud.logging
 from pydantic.error_wrappers import ErrorWrapper, ValidationError
 from starlette.requests import Request
 from starlette.responses import Response
@@ -12,6 +13,16 @@ from routes.anime.router import router as anime_router
 app = FastAPI()
 
 app.include_router(anime_router)
+
+
+# Instantiates a client
+client = google.cloud.logging.Client()
+
+# Retrieves a Cloud Logging handler based on the environment
+# you're running in and integrates the handler with the
+# Python logging module. By default, this captures all logs
+# at INFO level and higher
+client.setup_logging()
 
 
 @app.exception_handler(RequestValidationError)
