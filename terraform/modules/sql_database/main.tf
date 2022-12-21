6b5fe4a03391b85be56d8193a4e0c1642c756d2f
dc-mkacprzak <michael@datacose.com>
2022-12-21 16:22:48 +0100

    protect SQL Instance from deleting

--- terraform/modules/sql_database/main.tf
+++ terraform/modules/sql_database/main.tf
@@ -2,7 +2,7 @@
 resource "google_sql_database_instance" "database_instance" {
     name = var.db_instance_name
     database_version = "MYSQL_8_0"
-    deletion_protection = false
+    deletion_protection = true
     settings {
         tier = var.db_tier
         backup_configuration {
